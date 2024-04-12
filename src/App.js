2170228582c812c161600787cfac77f8dcb56582
dharm <singhdharm1008@gmail.com>
2024-04-12 19:16:49 +0530

    integrated PrimeReact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import PublicRoutes from "./routes/Public";
 import PrivateRoutes from "./routes/Private";
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 function App() {
   let isLoggedIn = false;
   const routes = useRoutes([{ path: "/*", element: isLoggedIn ? <PrivateRoutes /> : <PublicRoutes /> }]);
 
-  return <div>{routes}</div>;
+  return <PrimeReactProvider>{routes}</PrimeReactProvider>;
 }
 
 export default App;
